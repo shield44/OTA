484ce29da1a8b4cb191b2e23ba41277cc4bcf665
kiam001 <blablatupfen@gmail.com>
2021-07-25 01:52:28 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="kiam001"
+export GITHUB_EMAIL="blablatupfen@gmail.com"
 
-export device=""
+export device="vayu"
 
-export ROM=""
+export ROM="Sakura"
 export ROM_DIR=""
-export ROM_VERSION=""
+export ROM_VERSION="5.1"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://raw.githubusercontent.com/kiam001/relaeses_manifests/main/sakura-vayu.xml"
+export manifest_url="https://github.com/ProjectSakura/android"
+export rom_vendor_name="lineage"
+export branch="11"
 export bacon="bacon"
 export buildtype=""
-export clean=""
-export generate_incremental=""
+export clean="installclean"
+export generate_incremental="true"
 export upload_recovery=""
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="100"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="kiam001/releases"
 
 export timezone="UTC"

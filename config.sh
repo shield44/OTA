821a17b04422e3590490f0ba8df8b4fd006c1f17
kiam001 <blablatupfen@gmail.com>
2021-09-24 16:41:25 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-export GITHUB_USER="kiam001"
-export GITHUB_EMAIL="blablatupfen@gmail.com"
+export GITHUB_USER="geek0609"
+export GITHUB_EMAIL="astroashwin@outlook.com"
 
-export device="vayu"
+export device="ginkgo"
 
 export ROM="Sakura"
 export ROM_DIR="$WORKSPACE/../Sakura-Android-11"
 export ROM_VERSION="5.2"
 export official="false"
-export local_manifest_url="https://raw.githubusercontent.com/kiam001/relaeses_manifests/main/sakura-vayu.xml"
+export local_manifest_url="https://raw.githubusercontent.com/kiam001/relaeses_manifests/main/sakura-ginkgo.xml"
 export manifest_url="https://github.com/ProjectSakura/android"
 export rom_vendor_name="lineage"
 export branch="11"
 export bacon="bacon"
-export buildtype="userdebug"
+export buildtype="user"
 export clean="clean"
 export generate_incremental="false"
-export upload_recovery="true"
+export upload_recovery="false"
 
 export ccache="true"
 export ccache_size="100"
